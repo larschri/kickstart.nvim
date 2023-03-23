de42bbfe1da8c2a8743f9aa0211bf4357d1a029b
Lars Christian Jensen <larschri@pvv.ntnu.no>
2023-03-23 23:27:08 +0100

    darker color

--- init.lua
+++ init.lua
@@ -127,6 +127,7 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
+      require('onedark').setup({style = 'deep'})
       vim.cmd.colorscheme 'onedark'
     end,
   },
